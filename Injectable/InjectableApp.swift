2b74261ee142ca0d35d6466366148c454533bae6
Maciej Górecki <maciekgorecki@icloud.com>
2020-12-01 19:32:53 +0100

    Initial Commit

---
+++ Injectable/InjectableApp.swift
@@ -0,0 +1,17 @@
+//
+//  InjectableApp.swift
+//  Injectable
+//
+//  Created by Maciej Gorecki on 01/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct InjectableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
